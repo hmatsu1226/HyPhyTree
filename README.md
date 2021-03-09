6496b7503153ee1d0f905ea9545e7dd7e04b30df
松本拡高 <matsumoto@matsumotonoMacBook-Pro.local>
2021-03-09 10:03:59 +0900

    add new file

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ The following libraries are used.
 * MASS
 * ape
 * ROCR
+* RSpectra
 
 ## Full node embeddings
 
@@ -55,3 +56,16 @@ Rscript predicting_nearest_neighbor.R
 
 The rank of each embedding method for the i-th simulation tree are saved in "out/Result_NN_MDS_i.csv ", "out/Result_NN_H1_i.csv", and "out/Result_NN_H2_i.csv", respectively.
 The mean rank are saved in "out/Result_NN_MDS_all.csv ", "out/Result_NN_H1_all.csv", and "out/Result_NN_H2_all.csv", respectively.
+
+## Test code for imputation of missing distances 
+```
+Rscript test_imputation_H1.R 80
+Rscript test_imputation_H2.R 80
+```
+
+Two partial trees are derived from "data_imputation/tree_1.txt".
+These trees contain external nodes "data_imputation/idx1_L80.txt" and "data_imputation/idx2_L80.txt", respectively.
+80 external nodes (+ outgroup node) are included in both of the partial trees.
+The imputed distance matrix are saved in "out_imputation/optD_L80_H1.txt" and "out_imputation/optD_L80_H2.txt", respectively.
+
+
---
+++ test_imputation_H2.R
@@ -0,0 +1,56 @@
+library(ape)
+library(RSpectra)
+
+args <- commandArgs(trailingOnly = T)
+L <- as.numeric(args[1])
+
+tree <- read.tree(file="data_imputation/tree_1.txt")
+
+N <- 100 + 1 #Leaves num
+r <- 101 #outgroup idx
+
+myfn_H2 <- function(para){
+  for(i in 1:length(na_pairs)){
+    D2[na_pairs[[i]][1], na_pairs[[i]][2]] <- para[i]
+    D2[na_pairs[[i]][2], na_pairs[[i]][1]] <- para[i]
+  }
+  
+  H <- -exp(D2)
+  
+  return(-eigs_sym(H, 2, opts = list(retvec = FALSE), which="SA")$values[1])
+}
+
+#
+idx1 <- read.table(paste("data_imputation/idx1_L",L,".txt",sep=""))[,1]
+idx2 <- read.table(paste("data_imputation/idx2_L",L,".txt",sep=""))[,1]
+
+D <- matrix(NA, N, N)
+D[idx1, idx1] <- cophenetic(tree)[idx1, idx1]
+D[idx2, idx2] <- cophenetic(tree)[idx2, idx2]
+
+na_pairs <- list()
+for(i in 1:(N-1)){
+  for(j in (i+1):N){
+    if(is.na(D[i,j])){
+      na_pairs <- c(na_pairs, list(c(i,j)))
+    }
+  }
+}
+
+D2 <- D
+#initialization
+tmp <- c()
+for(i in 1:length(na_pairs)){
+  D2[na_pairs[[i]][1], na_pairs[[i]][2]] <- max(abs(D[na_pairs[[i]][1],]-D[na_pairs[[i]][2],]), na.rm=TRUE)
+  D2[na_pairs[[i]][2], na_pairs[[i]][1]] <- D2[na_pairs[[i]][1], na_pairs[[i]][2]]
+  tmp <- c(tmp, D2[na_pairs[[i]][1], na_pairs[[i]][2]])
+}
+
+optpara <- optim(tmp, myfn_H2, method="L-BFGS-B", lower=0, upper=2*max(D, na.rm = TRUE), control=list(trace=1, maxit=2000))
+print(optpara$value)
+for(i in 1:length(na_pairs)){
+  D2[na_pairs[[i]][1], na_pairs[[i]][2]] <- optpara$par[i]
+  D2[na_pairs[[i]][2], na_pairs[[i]][1]] <- optpara$par[i]
+}
+
+write.table(D2, paste("out_imputation/optD_L",L,"_H2.txt",sep=""), sep="\t", col.names=F, row.names=F)
